d1a6726a9550dddc3c650d0099fb624075de9b8e
GlebDontsov <dontsovggwp@mail.ru>
2023-03-12 11:46:28 +0300

    add models.py accessor.py

--- app/crm/routes.py
+++ app/crm/routes.py
@@ -1,6 +1,9 @@
-from aiohttp.web import Application
-from app.crm.views import index
+import typing
+
+if typing.TYPE_CHECKING:
+    from app.web.app import Application
 
 
-def setup_routes(app: Application):
-    app.router.add_get('/index', index)
+def setup_routes(app: "Application"):
+    from app.crm.views import AddUserView
+    app.router.add_view('/add_user', AddUserView)
